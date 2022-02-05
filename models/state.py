cda370ef327aa9890b190be13abcf8e903783115
Crisgrva <crstngranada@gmail.com>
2022-02-04 20:05:57 -0500

    [ Task 7 ] Create views to all default API actions to class State [ 02/04/2022/08:05 ]

--- models/state.py
+++ models/state.py
@@ -16,8 +16,7 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship("City",
                               backref="state",
-                              cascade="all, delete",
-                              passive_deletes=True)
+                              cascade="delete")
     else:
         name = ""
 
